ec740628eb6ea574cfba973047feda315e07ff95
Edward Omopariola <femiedjr@gmail.com>
2024-07-19 14:38:36 -0400

    basic html page with a button that perform as an onclick event

---
+++ script.js
@@ -0,0 +1,5 @@
+ function colorOrange() {
+    document.body.style.background="orange";
+    // since we are using a p tag we need to give it an index number [0], so we can give the p element a color
+    document.querySelectorAll("p")[0].style.color="red";
+  }
